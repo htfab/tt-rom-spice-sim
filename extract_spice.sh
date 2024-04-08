63644403215c9ae40209bf91ff409c86e17b53bf
Uri Shaked <uri@urishaked.com>
2024-04-08 10:57:34 +0300

    feat: ensure extracted port order is consistent

--- extract_spice.sh
+++ extract_spice.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+rm -rf extfiles
+
 magic -dnull -noconsole -rcfile $PDK_ROOT/sky130A/libs.tech/magic/sky130A.magicrc << EOF
 gds read tt_um_chip_rom.gds
 load tt_um_chip_rom
 select cell tt_um_chip_rom
+readspice tt_um_chip_rom_ports.spice
 extract path extfiles
 extract all
 ext2spice lvs
