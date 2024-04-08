9eb4f1ca54f1e4fb508f233e3799c0d2ff34f1ff
Uri Shaked <uri@urishaked.com>
2024-04-08 23:46:08 +0300

    fix: remove redundant line from magic script

--- extract_spice.sh
+++ extract_spice.sh
@@ -4,7 +4,6 @@ rm -rf extfiles
 magic -dnull -noconsole -rcfile $PDK_ROOT/sky130A/libs.tech/magic/sky130A.magicrc << EOF
 gds read tt_um_chip_rom.gds
 load tt_um_chip_rom
-select cell tt_um_chip_rom
 readspice tt_um_chip_rom_ports.spice
 extract path extfiles
 extract all
